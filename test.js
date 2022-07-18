0a971d765bbcabe544553932514b9be3ae123495
Travis Ridgway <ridgwaytravis@gmail.com>
2022-07-18 06:39:41 -0500

    Add node.js script to generate and style content within the DOM

---
+++ test.js
@@ -0,0 +1,42 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
+
+const para = document.createElement('p');
+content.classList.add('para');
+para.textContent = 'Hey I am red';
+para.style.cssText = 'color: red';
+container.appendChild(para);
+
+const hThree = document.createElement('h3');
+content.classList.add('hThree');
+hThree.textContent = 'I am a blue H3';
+hThree.style.cssText = 'color: blue';
+container.appendChild(hThree);
+
+const blackPink = document.createElement('div');
+content.classList.add('blackPink');
+blackPink.style.backgroundColor = 'pink';
+blackPink.style.border = '2px solid black';
+
+
+const hOne = document.createElement('h1');
+content.classList.add('hOne');
+hOne.textContent = 'I am in a div';
+
+
+const lilPara = document.createElement('p');
+content.classList.add('lilPara');
+lilPara.textContent = 'ME TOO';
+
+blackPink.appendChild(hOne);
+blackPink.appendChild(lilPara);
+container.appendChild(blackPink);
+
+
+
+ 
